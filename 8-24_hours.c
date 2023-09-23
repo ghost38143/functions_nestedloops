d33aedae45632c6dd7cc948fa50924596543ae7b
ghost38143 <ghost38143@gmail.com>
2023-09-23 15:38:22 -0700

    best code of of printing every minute of the day

---
+++ 8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - printing his every minute of his day
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int m;
+	int n;
+
+	for (m = 0; m < 24; m++)
+	{
+		for (n = 0; n < 60; n++)
+		{
+			_putchar ('0' + m / 10);
+			_putchar ('0' + m % 10);
+			_putchar (':');
+			_putchar ('0' + n / 10);
+			_putchar ('0' + n % 10);
+
+			if (m == 23 && n == 59)
+				continue;
+			_putchar('\n');
+		}
+	}
+}
